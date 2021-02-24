e44fef7fa5212d2aa2b66fbdf6fdbe73df167c40
AlreadyTakenJonas <jonas@eichhorn-networks.de>
2021-02-24 16:56:19 +0100

    Redid anaconda environment and added hello-world-programm

---
+++ easyExercise_01_helloWorld/helloWorld.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 19:29:45 2021
+
+@author: jonas
+"""
+
+# HELLO WORLD: Print a friendly message to the screen
+print("Hello, world!")
