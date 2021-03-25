a25544e25fb9e8efd960de5907ba2a00206463ff
AlreadyTakenJonas <jonas@eichhorn-networks.de>
2021-03-25 08:47:44 +0100

    Added data and empty python script for life coding another parsing exercise

---
+++ easyExercise_07_parsingFiles/easyExercise_07b_parsingFiles.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 08:46:15 2021
+
+@author: jonas
+"""
+
+
+"""
+
+    This program reads a file and converts the content of the file into an array.   
+
+"""
+
