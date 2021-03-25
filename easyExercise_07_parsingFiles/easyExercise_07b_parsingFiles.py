6a234be925e87f77991e08c8effeeff8ed8a3d37
AlreadyTakenJonas <jonas@eichhorn-networks.de>
2021-03-25 09:39:34 +0100

    Exercise 7b complete

--- easyExercise_07_parsingFiles/easyExercise_07b_parsingFiles.py
+++ easyExercise_07_parsingFiles/easyExercise_07b_parsingFiles.py
@@ -13,3 +13,32 @@ Created on Thu Mar 25 08:46:15 2021
 
 """
 
+
+from pathlib import Path
+import numpy as np   
+
+#
+#   READ THE FILE
+#
+data_file = Path("complicatedData.txt")
+raw_data = data_file.read_text()
+
+#
+#   PARSE FILE
+#
+
+# Splitting the data into an array of lines
+data = raw_data.splitlines()
+# Removing the header of the file; Each line of the header starts with '#'
+data = [ row for row in data if row[0] != "#" ]
+# Split each row into columns
+data = [ row.split("\t") for row in data ]
+# Convert every element into a floating point number
+converted_data = []
+for row in data:
+    row = [ float(number) for number in row ]
+    converted_data.append(row)
+    
+# Convert data into a numpy array
+converted_data = np.array(converted_data)
+
